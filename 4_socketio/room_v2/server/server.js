acaad9b0e79033a11d6203c9b3f633081698b670
hakyu <dlgkrbek123@gmail.com>
2023-11-09 17:29:44 +0900

    PART1 완료

---
+++ 4_socketio/room_v2/server/server.js
@@ -0,0 +1,40 @@
+const { Server } = require('socket.io');
+
+const io = new Server('5000', {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+const clients = new Map();
+
+io.sockets.on('connection', (socket) => {
+  socket.on('login', (data) => {
+    const { userId, roomNumber } = data;
+
+    socket.join(roomNumber);
+    clients.set(userId, socket.id);
+    socket.broadcast.emit('sLogin', userId);
+  });
+
+  socket.on('message', (res) => {
+    const { target } = res;
+
+    if (target) {
+      const toUser = clients.get(target);
+      io.sockets.to(toUser).emit('sMessage', res);
+      return;
+    }
+
+    const myRooms = Array.from(socket.rooms);
+    if (myRooms.length > 1) {
+      socket.broadcast.in(myRooms[1]).emit('sMessage', res);
+    } else {
+      socket.broadcast.emit('sMessage', res);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
